1ad491e8899cc6409b354f2a74ebc8b52d599265
Yaron Goldberg <yaron@run.ai>
2020-08-31 08:58:12 +0300

    sigopt

---
+++ partners/sigopt/remote.py
@@ -0,0 +1,21 @@
+
+from sigopt import Connection
+from sigopt.examples import franke_function
+import os
+import time
+
+experiment_id = os.getenv('EXP_ID')
+
+# Evaluate your model with the suggested parameter assignments
+# Franke function - http://www.sfu.ca/~ssurjano/franke2d.html
+def evaluate_model(assignments):
+  return franke_function(assignments['x'], assignments['y'])
+
+conn = Connection(client_token="YOHWYNKWGMWFFJVTBCOXBNMYQAYMCBUPINWIYVGLWHVXPPCB")
+time.sleep(60)
+suggestion = conn.experiments(experiment_id).suggestions().create()
+value = evaluate_model(suggestion.assignments)
+conn.experiments(experiment_id).observations().create(
+  suggestion=suggestion.id,
+  value=value,
+  )  
