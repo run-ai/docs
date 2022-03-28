df242c13656cd15d52e7c96704f7d61d1a6059ff
Erez Freiberger <enoodle@gmail.com>
2022-03-28 17:51:31 +0300

    support mpi 0.3.0

--- quickstart/distributed/entrypoint.sh
+++ quickstart/distributed/entrypoint.sh
@@ -6,4 +6,4 @@ if [ -z "$RUNAI_MPI_NUM_WORKERS" ]; then
 fi
 
 echo "Running with $RUNAI_MPI_NUM_WORKERS workers"
-horovodrun -np $RUNAI_MPI_NUM_WORKERS python scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=resnet20 --num_batches=1000000 --data_name cifar10 --data_dir /cifar10 --batch_size=64 --variable_update=horovod
+mpirun --allow-run-as-root -np $RUNAI_MPI_NUM_WORKERS -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib python scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=resnet20 --num_batches=1000000 --data_name cifar10 --data_dir /cifar10 --batch_size=64 --variable_update=horovod
