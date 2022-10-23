d399104438ceb3484c7cb4123e2ab2552cc9ecf0
Moran Guy <51398612+morangux@users.noreply.github.com>
2022-10-23 16:57:21 +0300

    Update kube-install.sh
    Fixed external IP curl command.

--- install/kube-install.sh
+++ install/kube-install.sh
@@ -71,7 +71,7 @@ function k8s-install {
 
 # *** init K8s
 function k8s-init {
-	server_ip=$(curl ifconfig.me)
+	server_ip=$(curl -s ifconfig.me)
 	echo -e "${GREEN} init k8s...${NC}"
         kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v${k8s_version} --token-ttl 186h --control-plane-endpoint=${server_ip}
         export KUBECONFIG=/etc/kubernetes/admin.conf
