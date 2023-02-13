88f991cdd363edc02c327f0f5b2fe32b2db369ea
Raz Rotenberg <raz.rotenberg@gmail.com>
2023-02-13 17:25:22 +0200

    Switched model to one that uses higher GPU utilization

--- quickstart/distributed/entrypoint-horovod-example.sh
+++ quickstart/distributed/entrypoint-horovod-example.sh
@@ -12,4 +12,4 @@ fi
 
 echo "Running with $RUNAI_MPI_NUM_WORKERS workers"
 # arguments taken from: https://github.com/kubeflow/mpi-operator/blob/v0.3.0/examples/v2beta1/tensorflow-benchmarks.yaml
-mpirun --allow-run-as-root -np $RUNAI_MPI_NUM_WORKERS -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib python /examples/tensorflow2_mnist.py
+mpirun --allow-run-as-root -np $RUNAI_MPI_NUM_WORKERS -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib python /examples/tensorflow2_synthetic_benchmark.py --num-batches-per-iter 10 --num-iters 1000
